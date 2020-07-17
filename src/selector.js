206c19b35373b9fed8d3c13f463d7ebd3b48123c
Katerina Solovyova <yekaterina.solovyova@gmail.com>
2020-07-16 21:07:53 -0400

    our first attempt

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,23 @@
 // YOUR CODE HERE
+class $ {
+    constructor (cssStr) {
+        let elementArray = [];
+    if (cssStr[0]==='.') {
+        elementArray = [...document.getElementsByClassName(cssStr)]
+    }
+
+    if (cssStr[0]==='#') {
+        elementArray = [...document.getElementById(cssStr)]
+    }
+
+    else {
+        elementArray = [...document.getElementsByTagName(cssStr)]
+    }
+    return elementArray;
+    }
+    
+}
+
+// document.getElementById()
+//document.getElementsByClassName('col-12')
+//document.getElementsByTagName('p')
