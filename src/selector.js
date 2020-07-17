180f37696e6c1b3a1fe5e66a63c8193c10a4f473
Katerina Solovyova <yekaterina.solovyova@gmail.com>
2020-07-16 23:45:14 -0400

    finalized js file for all tests to pass

--- src/selector.js
+++ src/selector.js
@@ -1,23 +1,91 @@
 // YOUR CODE HERE
 class $ {
-    constructor (cssStr) {
-        let elementArray = [];
-    if (cssStr[0]==='.') {
-        elementArray = [...document.getElementsByClassName(cssStr)]
+    constructor (selector) {
+              if (selector[0] === ('.')) {
+                this.elements = [...document.getElementsByClassName(selector.slice(1))];
+              } else if (selector.startsWith('#')) {
+                this.elements = [document.getElementById(selector.slice(1))]; // not a ... because you only get one element back
+              } else {
+                this.elements = [...document.getElementsByTagName(selector)];
+              }
+            }
+
+    hide() {
+        for (let i=0; i<this.elements.length; i++) {
+            let currElem = this.elements[i];
+            currElem.style.display = 'none'
+        }
+        //this.elements.forEach(elem => elem.style.display = 'none')
+        //return this;
     }
 
-    if (cssStr[0]==='#') {
-        elementArray = [...document.getElementById(cssStr)]
+    show() {
+        this.elements.forEach(elem => elem.style.display = "inherit")
+        return this;
     }
 
-    else {
-        elementArray = [...document.getElementsByTagName(cssStr)]
+    addClassName(className) {
+        this.elements.forEach(elem => elem.classList.add(className))
     }
-    return elementArray;
+
+    removeClassName(className) {
+        this.elements.forEach(elem => elem.classList.remove(className))
     }
-    
+
+    text(textToAdd) {
+        this.elements.forEach(elem => elem.innerText = textToAdd)
+    }
+
+    addChild(newDOM) {
+        this.elements.forEach(elem => elem.appendChild(document.createElement(newDOM)))
+    }
 }
 
-// document.getElementById()
-//document.getElementsByClassName('col-12')
-//document.getElementsByTagName('p')
+// class $ {
+//     constructor (selector) {
+//       if (selector.startsWith('.')) {
+//         this.elements = Array.from(document.getElementsByClassName(selector.slice(1)));
+//       } else if (selector.startsWith('#')) {
+//         this.elements = [document.getElementById(selector.slice(1))];
+//       } else {
+//         this.elements = Array.from(document.getElementsByTagName(selector));
+//       }
+//     }
+//     hide () {
+//       this.elements.forEach(element => {
+//         element.style.display = 'none';
+//       });
+//       return this;
+//     }
+//     show () {
+//       this.elements.forEach(element => {
+//         element.style.display = 'inherit';
+//       });
+//       return this;
+//     }
+//     addClassName (classToAdd) {
+//       this.elements.forEach(element => {
+//         element.classList.add(classToAdd);
+//       });
+//       return this;
+//     }
+//     removeClassName (classToRemove) {
+//       this.elements.forEach(element => {
+//         element.classList.remove(classToRemove);
+//       });
+//       return this;
+//     }
+//     text (newText) {
+//       this.elements.forEach(element => {
+//         element.innerText = newText;
+//       });
+//       return this;
+//     }
+//     addChild (elementType) {
+//       this.elements.forEach(element => {
+//         const childToAdd = document.createElement(elementType);
+//         element.appendChild(childToAdd);
+//       });
+//       return this;
+//     }
+//   }
